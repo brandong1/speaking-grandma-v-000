38d6195e42fecb90959307263cabbf59bdeb7383
Brandon Green <brandon@brandonleeproductions.com>
2018-05-20 21:20:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
   if phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   else 
