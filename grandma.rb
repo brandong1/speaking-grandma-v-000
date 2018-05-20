ace7803a41643ced56889d7a9df2206faf1b2951
Brandon Green <brandon@brandonleeproductions.com>
2018-05-20 21:03:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,8 +14,8 @@
 
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
-    return ""
-  if phrase != phrase.upcase
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase = phrase.upcase
     return "NO, NOT SINCE 1938!"
